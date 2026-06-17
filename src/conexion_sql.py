81b0f54dfa3f23f927a28b06a0661999ff50e006
Dome1108 <domenica.villalva.dalgo@udla.edu.ec>
2026-06-17 16:23:23 -0500

    Update conexion_sql.py

--- src/conexion_sql.py
+++ src/conexion_sql.py
@@ -1,1 +1,25 @@
+import os
+import pyodbc
+from dotenv import load_dotenv
 
+load_dotenv()
+
+
+def conectar_sql():
+    """
+    Crea conexión a SQL Server usando variables del archivo .env.
+    """
+
+    server = os.getenv("SQL_SERVER")
+    database = os.getenv("SQL_DATABASE")
+    driver = os.getenv("SQL_DRIVER", "ODBC Driver 17 for SQL Server")
+
+    conn_str = (
+        f"DRIVER={{{driver}}};"
+        f"SERVER={server};"
+        f"DATABASE={database};"
+        "Trusted_Connection=yes;"
+        "TrustServerCertificate=yes;"
+    )
+
+    return pyodbc.connect(conn_str)
